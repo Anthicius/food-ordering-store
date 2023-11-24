c5dbf5d18785627bb7eb50f7c44efa04cc3c2abc
Anthicius <andrius.makseckas@gmail.com>
2023-11-24 11:15:57 +0200

    first commit

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children} </div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return <div>Modal</div>;
+};
+
+export default Modal;
